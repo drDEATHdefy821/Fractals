22eee9c5f3e0ff33c5110510edd8c4050e76b593
drDEATHdefy821 <christopher.braziel@googlemail.com>
2021-05-07 21:54:24 +0100

    V2

--- sketch.js
+++ sketch.js
@@ -1,31 +1,30 @@
-let slider;
+var tree = [];
+
 function setup() {
   // put setup code here
   createCanvas(400,400);
-  slider = createSlider(0, TWO_PI, PI/4, 0.01);
+  var a = createVector(width/2, height);
+  var b = createVector(width/2, height - 100);
+  var root = new Branch(a, b);
+
+  tree[0] = root;
+}
+
+function mousePressed() {
+  for (var i = tree.length-1; i >= 0; i--) {
+    if (!tree[i].finished) {
+      tree.push(tree[i].branchA());
+      tree.push(tree[i].branchB());
+    }
+    tree[i].finished = true;
+  }
 }
 
 function draw() {
   // put drawing code here
   background(51);
-  stroke(255);
-  translate(200, height);
-  branch(100);
-}
 
-function branch(len) {
-  let angle = slider.value();
-  line(0, 0, 0, 0 - len);
-  translate(0, - len);
-
-  if (len > 4) {
-    push();
-    rotate(angle);
-    branch(len * 0.67);
-    pop();
-    push();
-    rotate(-angle);
-    branch(len * 0.67);
-    pop();
+  for (var i = 0; i < tree.length; i++) {
+    tree[i].show();
   }
 }
